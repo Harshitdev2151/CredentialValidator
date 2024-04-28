32bf7583670632984ceea1e3b85f3dfa2589a8eb
Harshit <harshit.2151.dev@gmail.com>
2024-04-28 22:47:58 +0530

    Initial commit

---
+++ CredentialValidator/CredentialValidator.h
@@ -0,0 +1,18 @@
+//
+//  CredentialValidator.h
+//  CredentialValidator
+//
+//  Created by Harshit Kumar on 28/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CredentialValidator.
+FOUNDATION_EXPORT double CredentialValidatorVersionNumber;
+
+//! Project version string for CredentialValidator.
+FOUNDATION_EXPORT const unsigned char CredentialValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CredentialValidator/PublicHeader.h>
+
+
---
+++ CredentialValidator/MyLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  MyLibrary.swift
+//  MyPrivateLibrary
+//
+//  Created by Harshit Kumar on 28/04/24.
+//
+
+import Foundation
+
+
+public class MyLibrary {
+    public class func dispalyMessage() -> String {
+        return "Amit + Harshit " 
+    }
+}
